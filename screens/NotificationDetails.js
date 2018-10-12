bf3999df5010141acd1ecba1c98a534f55ece9b8
Mukesh smk <mukeshsmk45@gmail.com>
2018-10-12 14:18:09 +0530

    Redesign Matchpage and Details page

--- screens/NotificationDetails.js
+++ screens/NotificationDetails.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { View, Text,StyleSheet,ScrollView,keyString,Button } from "react-native";
+import { View, Image,Text,StyleSheet,ScrollView,keyString,Button } from "react-native";
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
-
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { Icon } from 'native-base';
 class NotificationDetails extends Component {
     static navigationOptions = {
         title: 'Notification Details',
@@ -15,18 +16,44 @@ class NotificationDetails extends Component {
  
                 <Card>
                 <ScrollView>
-                <CardTitle 
-                  title="Android Developer"                 
-                />
-                 <Text style={styles.CompanyName}>Company Name- Chennai</Text>
-                 <CardTitle 
+                        <Card>
+                        <Grid style={styles.Compydetails}>
+                            <Col size={20}><Text style={styles.Iconimg}><Image
+							style={styles.editImage}
+							source={{uri: 'https://www.torrejoncillotodonoticias.com/wp-content/uploads/2011/05/android-icon.png'}}
+							
+						/>	</Text></Col>
+                            <Col size={75} style={{marginLeft:'3%'}}>
+                            <Text style={styles.JobName}>Andriod Developer</Text>       
+                            <Text style={styles.CompanyName}>IRtrends pvt ltd</Text>
+                            <Text style={styles.CompanyAddress}>Company Name- Chennai</Text></Col>
+                        </Grid>   
+                        <Grid>
+                            <Col style={styles.salaryDetails}>
+                            <Icon name='ios-briefcase' style={{color:'#384850'}}/>
+					
+                            <Text style={styles.CompanyName}>0-10 yrs</Text>  
+                            </Col>
+                            <Col style={styles.salaryDetails}>
+                            <Icon name="ios-pin" style={{color:'#384850'}}/>
+                            <Text style={styles.CompanyName}>Chennai</Text>  
+                            </Col>
+                            <Col style={styles.salaryDetails}>
+                            <Icon name="ios-cash" style={{color:'#384850'}}/>
+                            <Text style={styles.CompanyName}>10k-15k</Text>  
+                            </Col>
+                        </Grid>      
+                        
+                      </Card>
+
+                 <CardTitle style={styles.Subtitle}
                   subtitle="Job Description"                 
                 />
 
                 <Text style={styles.JobSummery}>We are looking for an Android developer responsible for
                  the development and maintenance of applications. To ensure the best possible performance, 
                  quality, and responsiveness of the application</Text>
-                 <CardTitle                  
+                 <CardTitle style={styles.Subtitle}                  
                     subtitle="Responsibilities and Duties"
                 />
                 <View style={ styles.column }>
@@ -84,13 +111,18 @@ class NotificationDetails extends Component {
                     </View>
 
                 </View>
-                <CardTitle
+                <CardTitle style={styles.Subtitle}
+                    subtitle="Skills"
+                />
+                <Text style={ styles.Skills }>Html5,Java script,Angular js,Node js</Text>
+                
+                <CardTitle style={styles.Subtitle}
                     subtitle="Experience"
                 />
                 <Text style={ styles.Experience }>Android development: 1 year (Required)</Text>
                 <Text style={ styles.Experience }>Java: 1 year (Required)</Text>
 
-                <CardTitle
+                <CardTitle style={styles.Subtitle}
                     subtitle="Education"
                 />
                 <Text style={ styles.Education }>Bachelor's (Required)</Text>
@@ -136,9 +168,26 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center'
     },
-    CompanyName:{
-        fontWeight: 'bold',
+    Compydetails:{
+        marginTop:'5%',
         marginLeft:'5%',
+    }
+    ,
+    JobName:{
+        fontSize: 18,
+        fontWeight: 'bold',
+      },
+      CompanyName:{
+        fontSize: 15,
+      },
+    
+      CompanyAddress:{
+        fontSize: 12,
+        color:'#194D33',
+      },
+    salaryDetails:{
+        alignItems: 'center',
+         margin:'5%',
     },
     ApplyButton: {
         color:'red',
@@ -151,6 +200,11 @@ const styles = StyleSheet.create({
     Roles:{
         color:'#000',
         padding:'3%',
+    },
+    Subtitle:{
+        backgroundColor: "#D7D5DB",
+        marginBottom:'2%',
+        marginTop:'2%',
     },
     column: {
         flexDirection: 'column',
@@ -179,6 +233,11 @@ const styles = StyleSheet.create({
         marginLeft:'8%',
         marginBottom:'3%',
     },
+    Skills:{
+        marginLeft:'8%',
+        marginBottom:'3%',
+        padding:'3%',
+    },
     ButtonRow: {
         flex: 1,
         flexDirection: 'row',
@@ -188,6 +247,14 @@ const styles = StyleSheet.create({
       ButtonColumn: {
         width: '40%',
         height: 40
-      }
-    
+      },
+      editImage:{
+        width:100,
+        height:100,  
+    },
+    Iconimg:{
+        textAlign:'center',
+        padding:'5%',
+        marginTop:'8%',
+    }
 });
